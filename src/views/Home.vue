8e1d131d69e4d4492dc9f9a68764445f66017df6
maximka777 <maksim.borovskij@yandex.ru>
2019-12-26 21:04:47 +0100

    Technologies; Projects

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -60,8 +60,241 @@
                 <b-icon pack="fab" icon="stack-overflow" size="is-medium" />
               </a>
             </li>
+            <li>
+              <a
+                href="https://www.producthunt.com/@mborovskij/made"
+                class="social-link"
+              >
+                <b-icon
+                  pack="fab"
+                  icon="product-hunt"
+                  size="is-medium"
+                  title="Product hunt"
+                />
+              </a>
+            </li>
             <!-- TODO: Add habr link -->
           </ul>
+        </div>
+      </div>
+    </section>
+
+    <section class="section">
+      <div class="container">
+        <h1 class="title">Projects I am/was working on</h1>
+        <div class="projects columns">
+          <div class="project-item column">
+            <div class="logo-container">
+              <img src="../assets/images/vs.png" />
+            </div>
+            <h3><a href="https://app.visualserver.com/">Visual Server</a></h3>
+            <p>Website builder for artists</p>
+            <span>Node.js full-stack developer</span><br />
+            <span>2017-2018</span>
+          </div>
+          <div class="project-item column">
+            <div class="logo-container">
+              <img src="../assets/images/et.png" />
+            </div>
+            <h3><a href="https://earthtoday.com">EarthToday</a></h3>
+            <p>
+              EarthToday is a community-driven content platform to inform,
+              inspire, educate and entertain.
+            </p>
+            <span>2018-now</span>
+          </div>
+          <div class="project-item column">
+            <div class="logo-container">
+              <img src="../assets/images/hh.svg" />
+            </div>
+            <h3><a href="https://hearhear.app">HearHear</a></h3>
+            <p>Ask anybody, anything. Together, we will get an answer.</p>
+            <span>2019-now</span>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="section">
+      <div class="container">
+        <h1 class="title">Technologies I touched to</h1>
+
+        <div class="technologies columns">
+          <h2 class="column subtitle">Languages and platforms</h2>
+        </div>
+        <div class="technologies columns">
+          <div class="technology-item column">
+            <h3>JavaScript</h3>
+            <b-progress
+              type="is-success"
+              size="small"
+              :value="95"
+              :show-value="false"
+            />
+          </div>
+          <div class="technology-item column">
+            <h3>Java</h3>
+            <b-progress
+              type="is-success"
+              size="small"
+              :value="90"
+              :show-value="false"
+            />
+          </div>
+          <div class="technology-item column">
+            <h3>Python</h3>
+            <b-progress
+              type="is-success"
+              size="small"
+              :value="60"
+              :show-value="false"
+            />
+          </div>
+          <div class="technology-item column">
+            <h3>Go</h3>
+            <b-progress
+              type="is-success"
+              size="small"
+              :value="55"
+              :show-value="false"
+            />
+          </div>
+        </div>
+        <div class="columns">
+          <div class="column">
+            <h3>
+              Node.js, TypeScript, Kotlin, bash, VHDL, R, C, C++, Assembler,
+              Swift, C#, ...
+            </h3>
+          </div>
+        </div>
+
+        <div class="technologies columns">
+          <h2 class="column subtitle">Client web frameworks and libraries</h2>
+        </div>
+        <div class="technologies columns">
+          <div class="technology-item column">
+            <h3>Vue</h3>
+            <b-progress
+              type="is-success"
+              size="small"
+              :value="90"
+              :show-value="false"
+            />
+          </div>
+          <div class="technology-item column">
+            <h3>Angular</h3>
+            <b-progress
+              type="is-success"
+              size="small"
+              :value="90"
+              :show-value="false"
+            />
+          </div>
+          <div class="technology-item column">
+            <h3>React</h3>
+            <b-progress
+              type="is-success"
+              size="small"
+              :value="60"
+              :show-value="false"
+            />
+          </div>
+          <div class="technology-item column" />
+        </div>
+
+        <div class="technologies columns">
+          <h2 class="column subtitle">Server web frameworks and libraries</h2>
+        </div>
+        <div class="technologies columns">
+          <div class="technology-item column">
+            <h3>Spring</h3>
+            <b-progress
+              type="is-success"
+              size="small"
+              :value="90"
+              :show-value="false"
+            />
+          </div>
+          <div class="technology-item column">
+            <h3>Express</h3>
+            <b-progress
+              type="is-success"
+              size="small"
+              :value="90"
+              :show-value="false"
+            />
+          </div>
+          <div class="technology-item column" />
+          <div class="technology-item column" />
+        </div>
+
+        <div class="technologies columns">
+          <h2 class="column subtitle">Storage</h2>
+        </div>
+        <div class="technologies columns">
+          <div class="technology-item column">
+            <h3>Cassandra</h3>
+            <b-progress
+              type="is-success"
+              size="small"
+              :value="90"
+              :show-value="false"
+            />
+          </div>
+          <div class="technology-item column">
+            <h3>Neo4j</h3>
+            <b-progress
+              type="is-success"
+              size="small"
+              :value="90"
+              :show-value="false"
+            />
+          </div>
+          <div class="technology-item column">
+            <h3>MongoDB</h3>
+            <b-progress
+              type="is-success"
+              size="small"
+              :value="80"
+              :show-value="false"
+            />
+          </div>
+          <div class="technology-item column">
+            <h3>MSSQL</h3>
+            <b-progress
+              type="is-success"
+              size="small"
+              :value="75"
+              :show-value="false"
+            />
+          </div>
+        </div>
+        <div class="columns">
+          <div class="column">
+            <h3>Redis, Hazelcast, PostgreSQL, ...</h3>
+          </div>
+        </div>
+
+        <div class="technologies columns">
+          <h2 class="column subtitle">Infrastructure</h2>
+        </div>
+        <div class="columns">
+          <div class="column">
+            <h3>
+              Google Cloud, SaltStack, Terraform, Nomad, Consul, Kafka, Grafana,
+              Prometheus, ...
+            </h3>
+          </div>
+        </div>
+
+        <div class="technologies columns">
+          <h2 class="column subtitle">Other thingies</h2>
+        </div>
+        <div class="columns">
+          <div class="column">
+            <h3>Linux, Git, MacOS, iOS, Android, ...</h3>
+          </div>
         </div>
       </div>
     </section>
@@ -118,6 +351,18 @@ export default {
       &:hover {
         color: $color-yellow;
       }
+    }
+  }
+}
+
+.project-item {
+  .logo-container {
+    display: flex;
+    justify-content: center;
+    margin-bottom: 0.5rem;
+
+    img {
+      height: 3rem;
     }
   }
 }
