a0714eef85bbca4b5556b2fd3387c697cd5bb6bd
maximka777 <maksim.borovskij@yandex.ru>
2020-01-04 19:28:58 +0100

    Trying to resolve all unnmatched paths with default page

--- prepare-for-publish.sh
+++ prepare-for-publish.sh
@@ -1,3 +1,6 @@
 rm -rf docs
 yarn build
 mv dist docs
+# to resolve any unmatched path with index.html
+# https://github.com/isaacs/github/issues/408#issuecomment-114326735
+cp docs/index.html docs/200.html
