fce08653e1df00055cbd9e2fd15957d27648f9d1
maximka777 <maksim.borovskij@yandex.ru>
2020-03-18 18:55:26 +0100

    New articles is added

--- src/views/MyArticles.vue
+++ src/views/MyArticles.vue
@@ -5,14 +5,24 @@
         <PageHeader :title="'My articles'" />
         <div class="articles">
           <div class="article">
-            <a href="https://habr.com/ru/post/429472/" target="_blank"
-              >Docker Compose + Consul + Spring Boot + FeignClient</a
+            <span>18 Mar 2020 - </span>
+            <a
+              href="https://medium.com/@mbarouski/google-compute-engine-load-balancing-with-consul-da3ada21d010"
+              target="_blank"
+              >Google Compute Engine load balancing with Consul</a
             >
           </div>
           <div class="article">
+            <span>31 Dec 2018 - </span>
             <a href="https://habr.com/ru/post/434902/" target="_blank"
               >Создание кастомного генератора запросов в Spring Data Neo4j
               (Часть 1)</a
+            >
+          </div>
+          <div class="article">
+            <span>11 Nov 2018 - </span>
+            <a href="https://habr.com/ru/post/429472/" target="_blank"
+              >Docker Compose + Consul + Spring Boot + FeignClient</a
             >
           </div>
         </div>
