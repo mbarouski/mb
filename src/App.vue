c3f8a1d0f2df3c05df6aa688fd9ba61fbf62dd91
maximka777 <maksim.borovskij@yandex.ru>
2020-06-16 13:53:44 +0300

    wip

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,25 @@
 <template>
   <div id="app">
     <router-view />
+    <div class="wip-label">work in progress</div>
   </div>
 </template>
 
-<style></style>
+<style>
+#app {
+  position: relative;
+}
+
+#app .wip-label {
+  position: absolute;
+  top: 0;
+  right: 0;
+  background: #ffdd57;
+  color: #7957d5;
+  transform: translate(10rem, 3rem) rotate(45deg);
+  padding: 0.25rem 10rem;
+  text-align: center;
+  border: 2px dashed;
+  white-space: nowrap;
+}
+</style>
